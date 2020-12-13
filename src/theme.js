58961a1b803f50d389e839ee1b72f2185476b0a6
lenakernytska <yaapril222@gmail.com>
2020-12-13 22:05:52 +0300

    Добавляет смену темы

---
+++ src/theme.js
@@ -0,0 +1,30 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const body=document.querySelector("body");
+const checkBox = document.querySelector(".theme-switch__toggle");
+
+
+checkBox.addEventListener('change', themeHandler);
+
+function themeHandler(event) {
+    console.log(event);
+    if (checkBox.checked) {
+         body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+         localStorage.setItem('theme', 'dark-theme'); 
+    } else if (!checkBox.checked) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme')
+        localStorage.setItem('theme', 'light-theme'); 
+    }
+};
+    
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.classList.add(currentTheme);
+};
+if (currentTheme === "dark-theme") {
+    checkBox.checked=true;
+}
